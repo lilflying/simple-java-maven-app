f96ffaf61d59f2b918445fe93578222b5d37fb17
Lil_flying <98503582+lilflying@users.noreply.github.com>
2026-06-22 16:10:53 +0200

    test 5

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -5,7 +5,7 @@ package com.mycompany.app;
  */
 public class App {
 
-    private static final String MESSAGE = "Hello World! Time to shine, for real this time";
+    private static final String MESSAGE = "Hello World! Time to shine, for real this time.";
 
     public App() {}
 
